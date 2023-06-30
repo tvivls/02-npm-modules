9f5b93f60387f3978e1ebe76b1e9cc6b8c9d4b2d
Валентина Чередниченко <vcherednichenko@web-bee.ru>
2023-06-30 23:46:14 +0300

    feat: добавлен reacr hook useMediaQuery

---
+++ react-responsive/src/hooks/useMediaQuery.ts
@@ -0,0 +1,20 @@
+import { useState, useEffect } from 'react';
+
+export const useMediaQuery = (options: { query: string }): boolean => {
+    const { query } = options;
+    const [matches, setMatches] = useState(false);
+
+    useEffect(() => {
+        const queryList = window.matchMedia(query);
+        const handleMediaQuery = (event: MediaQueryListEvent): void => {
+            setMatches(event.matches);
+        };
+        queryList.addEventListener('change', handleMediaQuery);
+        setMatches(queryList.matches);
+        return () => {
+            queryList.removeEventListener('change', handleMediaQuery);
+        };
+    }, [query])
+
+    return matches;
+};
