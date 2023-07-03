543520690ffd838888892467789ff64236409437
Валентина Чередниченко <vcherednichenko@web-bee.ru>
2023-07-03 17:56:41 +0300

    fix (MediaQuery): сокращен код

--- react-responsive/src/components/MediaQuery.tsx
+++ react-responsive/src/components/MediaQuery.tsx
@@ -1,66 +1,49 @@
-import React, { FC, ReactNode, useEffect, useState } from 'react';
+import React, { FC, Fragment, ReactNode, useEffect, useState } from 'react';
 
-export type QueryProps = { orientation?: string;
-    minResolution?: string | number;
-    maxResolution?: string;
-    minWidth?: number;
-    maxWidth?: number;
-    minHeight?: number;
-    maxHeight?: number;
-    children?: ReactNode | ((matches: boolean) => ReactNode);
+export type QueryProps = {
+  orientation?: string;
+  minResolution?: string | number;
+  maxResolution?: string;
+  minWidth?: number;
+  maxWidth?: number;
+  minHeight?: number;
+  maxHeight?: number;
+  children?: ReactNode | ((matches: boolean) => ReactNode);
 };
 
-const MediaQuery: FC<QueryProps> = ({children, ...props}) => {
-    const [matches, setMatches] = useState(false);
-    useEffect(() => {
-        const query = buildQuery();
-        const queryList = window.matchMedia(query);
-        const handleMediaQuery = (event: MediaQueryListEvent) => {
-            setMatches(event.matches);
-        };
-        queryList.addEventListener('change', handleMediaQuery);
-        setMatches(queryList.matches);
-        return () => {
-            queryList.removeEventListener('change', handleMediaQuery);
-        };
-    }, []);
-
-    const buildQuery = (): string => {
-        const mediaQuery: string[] = [];
-        Object.entries(props).forEach(([key, value]) => {
-            if (typeof value === 'number' || typeof value === 'string') {
-                const splitString: string[] = /(?=[A-Z])/g[Symbol.split](key);
-                const formattedKey: string = splitString.join('-').toLowerCase();
-                const queryString = `(${formattedKey}: ${value}`;
-                if (key.includes('orientation')) {
-                    mediaQuery.push(`${queryString})`);
-                } else if (key.includes('Resolution')) {
-                    if (formattedKey === 'min-resolution') {
-                        switch (typeof value) {
-                            case 'number':
-                                mediaQuery.push(`${queryString}dppx)`);
-                                break;
-                            case 'string':
-                                mediaQuery.push(`${queryString})`);
-                                break;
-                        }
-                    } else {
-                        mediaQuery.push(`${queryString})`);
-                    }
-                } else {
-                    mediaQuery.push(`${queryString}px)`);
-                }
-            }
-        });
-        return mediaQuery.join(' and ');
+const MediaQuery: FC<QueryProps> = ({ children, ...props }) => {
+  const [matches, setMatches] = useState(false);
+  useEffect(() => {
+    const query = buildQuery();
+    const queryList = window.matchMedia(query);
+    const handleMediaQuery = (event: MediaQueryListEvent) => {
+      setMatches(event.matches);
     };
-    return (
-        <React.Fragment>
-            {typeof children === 'function'
-                ? (children as (matches: boolean) => React.ReactNode)(matches)
-                : (matches && <div>{children}</div>)}
-        </React.Fragment>
-    );
+    queryList.addEventListener('change', handleMediaQuery);
+    setMatches(queryList.matches);
+    return () => {
+      queryList.removeEventListener('change', handleMediaQuery);
+    };
+  }, []);
+
+  const buildQuery = (): string => {
+    const mediaQuery: string[] = Object.entries(props).map(([key, value]) => {
+      const formattedKey: string = key.replace(/([A-Z])/g, '-$1').toLowerCase();
+      const queryString = `(${formattedKey}: ${value}`;
+      if (formattedKey === 'orientation') return `${queryString})`;
+      if (formattedKey.includes('resolution'))
+        return typeof value === 'number' ? `${queryString}dppx)` : `${queryString})`;
+      else return `${queryString}px)`;
+    });
+    return mediaQuery.join(' and ');
+  };
+  return (
+    <>
+      {typeof children === 'function'
+        ? (children as (matches: boolean) => React.ReactNode)(matches)
+        : matches && <>{children}</>}
+    </>
+  );
 };
 
 export default MediaQuery;
