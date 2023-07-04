5fddfd08d18846c16154e5ba74e9c8e7c4d42f9a
Валентина Чередниченко <vcherednichenko@web-bee.ru>
2023-07-04 15:35:38 +0300

    fix (useDocumentVisibility.ts): исправлен react hook useDocumentVisibility

--- react-document-visibility/src/hooks/useDocumentVisibility.ts
+++ react-document-visibility/src/hooks/useDocumentVisibility.ts
@@ -1,30 +1,34 @@
-import {useEffect, useState} from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 export const useDocumentVisibility = () => {
-    const [visible, setVisible] = useState(true);
-    const [count, setCount] = useState(0);
+  const [visible, setVisible] = useState<boolean>(document.visibilityState === 'visible');
+  const [count, setCount] = useState(0);
+  const callbackArray = useRef<((visibility: boolean) => void)[]>([]);
+
+  const onVisibilityChange = (callback: (visibility: boolean) => void): void => {
+    callbackArray.current.push(callback);
+  };
+
+  useEffect(() => {
     const handleVisibility = (): void => {
-        if (document.visibilityState === 'hidden') {
-            setCount(prevState => prevState + 1);
-            setVisible(false);
-        } else {
-            setVisible(true);
-        }
+      callbackArray.current.forEach((currentCallback) => currentCallback(document.visibilityState === 'visible'));
+      if (document.visibilityState === 'hidden') {
+        setCount((prevState) => prevState + 1);
+        setVisible(false);
+      } else {
+        setVisible(true);
+      }
     };
-    useEffect(() => {
-        document.addEventListener('visibilitychange', handleVisibility);
-        return () => {
-            document.removeEventListener('visibilitychange', handleVisibility);
-        };
-    }, []);
-    return {
-        count,
-        visible,
-        onVisibilityChange: (callback: EventListener) => {
-            document.addEventListener('visibilitychange', callback);
-            return () => {
-                document.removeEventListener('visibilitychange', callback);
-            };
-        },
+
+    document.addEventListener('visibilitychange', handleVisibility);
+    return () => {
+      document.removeEventListener('visibilitychange', handleVisibility);
     };
+  }, []);
+
+  return {
+    count,
+    visible,
+    onVisibilityChange,
+  };
 };
