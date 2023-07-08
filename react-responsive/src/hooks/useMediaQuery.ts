3cfb28cad85aa847990dac4d47156ff05507214c
Валентина Чередниченко <vcherednichenko@web-bee.ru>
2023-07-08 15:31:53 +0300

    feat (useMediaQuery): добалена поддержка ssr

--- react-responsive/src/hooks/useMediaQuery.ts
+++ react-responsive/src/hooks/useMediaQuery.ts
@@ -1,20 +1,24 @@
 import { useState, useEffect } from 'react';
 
-export const useMediaQuery = (options: { query: string }): boolean => {
-    const { query } = options;
-    const [matches, setMatches] = useState(false);
+export type QueryType = {
+  query: string;
+};
 
+export const useMediaQuery = ({ query }: QueryType, isSSR = false) => {
+  const [matches, setMatches] = useState(false);
+  if (!isSSR) {
     useEffect(() => {
-        const queryList = window.matchMedia(query);
-        const handleMediaQuery = (event: MediaQueryListEvent): void => {
-            setMatches(event.matches);
-        };
-        queryList.addEventListener('change', handleMediaQuery);
-        setMatches(queryList.matches);
-        return () => {
-            queryList.removeEventListener('change', handleMediaQuery);
-        };
+      const queryList = window.matchMedia(query);
+      const handleMediaQuery = (event: MediaQueryListEvent) => {
+        setMatches(event.matches);
+      };
+      queryList.addEventListener('change', handleMediaQuery);
+      setMatches(queryList.matches);
+      return () => {
+        queryList.removeEventListener('change', handleMediaQuery);
+      };
     }, [query]);
+  }
 
-    return matches;
+  return matches;
 };
